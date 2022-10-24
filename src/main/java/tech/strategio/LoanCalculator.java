324ce46c34a77e4e0d238d9563502d7689dfaff2
Kelly <kellygreentea32@gmail.com>
2022-10-24 17:39:30 -0400

    Completed todos in LoanCalculator.java

--- src/main/java/tech/strategio/LoanCalculator.java
+++ src/main/java/tech/strategio/LoanCalculator.java
@@ -3,16 +3,22 @@ package tech.strategio;
 import java.util.Scanner;
 
 public class LoanCalculator {
-
     /**
-     * TODO: finish out this JavaDoc comment block.
-     * FIXME: WHAT DOES THIS METHOD DO?
-     * 
-     * @param ???
-     * @return ???
+     * This method receives a total loan amount ("amount") as a parameter,
+     * then calculates how much will still be owed after 3 months
+     * if 10% is paid each month. The result of these calculations
+     * is returned ("amount").
+     *
+     * @param amount the total loan amount before calculations
+     * @return       the remaining loan amount after 3 months of 10% payments
      */
     static int getRemainingAmountIn3Months(int amount) {
-        // TODO: Add your code below
+        for(int i=0; i < 3; i++){
+            int payment = (amount * 10) / 100;
+            int remaining = amount - payment;
+            amount = remaining;
+        }
+        return amount;
     }
 
     public static void main(String[] args) {
